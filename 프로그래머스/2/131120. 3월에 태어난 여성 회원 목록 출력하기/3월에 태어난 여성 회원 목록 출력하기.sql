22ef0f54f86da81adf3e88a8a8c8e5258b919948
Yonghun Shin <117963215+choam2426@users.noreply.github.com>
2025-12-15 21:41:23 +0900

    Add SQL query for March female members
    Added SQL query to select female members born in March.

--- 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -1,2 +1,2 @@
--- 코드를 입력하세요
+# https://school.programmers.co.kr/learn/courses/30/lessons/131120
 SELECT MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d') as DATE_OF_BIRTH FROM MEMBER_PROFILE WHERE GENDER = "W" and MONTH(DATE_OF_BIRTH) = 3 and TLNO IS NOT NULL ORDER BY MEMBER_ID
